efa6c3cadfda14876393ccc4f614544f32c2f136
Douglas Miranda <douglasmvc@hotmail.com>
2024-02-27 21:11:35 -0300

    Projeto Cardapio

---
+++ restaurante/src/App.jsx
@@ -0,0 +1,29 @@
+
+import { useState } from "react";
+import Restaurante from "./assets/restaurante.webp"
+import "./App.css";
+import {Navegacao} from "./Navegacao";
+import { ItemCardapio } from "./ItemCardapio";
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio";
+
+const paginaSelecionada = 0;
+
+export function App(){
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
+
+  return <>
+         <img src={Restaurante} alt="" className="capa"/>
+         <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+         <div className="menu">
+            {paginasMenu[paginaSelecionada].map(item => <ItemCardapio 
+            nome={item.nome} 
+            descricao={item.descricao}
+            preco={item.preco}
+            imagem={item.imagem}
+            />)}
+         </div>
+          
+         </>
+}
